18516064e03f15697b1afcbce95a468b667110fb
Hiroshan Madusanka <pmhiroshan@gmail.com>
2024-10-20 18:24:35 +0530

    Tailwin css install

--- hotel-management-frontend/src/assets/componentes/header/header.jsx
+++ hotel-management-frontend/src/assets/componentes/header/header.jsx
@@ -1,11 +1,12 @@
 import UserTag from "../userData/userData.jsx";
-import "./header.css";
+
 
 function Header(){
 
     return(
-        <header>
-            <h1>React Header</h1>
+        <header className="w-full bg-blue-500 flex h-[100px] 
+        relative items-center">
+            <h1 className="text-white text-[30px]">Hotel Manegment System</h1>
             <UserTag imageLink="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png" 
             name="Hiroshan Madusanka"/>
             
