cc0b1754ceb40509b526b49682a03a892fa11817
Hiroshan Madusanka <pmhiroshan@gmail.com>
2024-10-24 11:36:07 +0530

    route create

--- hotel-management-frontend/src/App.jsx
+++ hotel-management-frontend/src/App.jsx
@@ -1,17 +1,26 @@
 import Header from "./assets/componentes/header/header.jsx"
 import './App.css'
 import './index.css'
+import AdminPage from "./assets/componentes/pages/admin.jsx"
 import HomePage from "./pages/client-page/homePage.jsx"
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   
 
   return (
     <>
-       <HomePage/>
-
-       
+      <BrowserRouter>
+        <Routes path="/*">
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
+          <Route path="/*" element={
+            <div className="w-full h-[100vh] bg-green-400">
+              404
+            </div>
+            } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ hotel-management-frontend/src/assets/componentes/pages/admin.jsx
@@ -0,0 +1,10 @@
+export default function AdminPage(){
+
+
+    return(
+        <div className="w-full h-[100vh] bg-yellow-400">
+            
+        </div>
+    )
+
+}
